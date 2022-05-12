9fccefa6fdf54a79e2674076749ea0fb2bd659e0
PRAMODSHENDAGE <shendagepramod@gmail.com>
2022-05-12 14:38:37 +0530

    Add project files.

---
+++ Line_Comparison_Problem/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Line_Comparison_Problem;
+Console.WriteLine("Welcome to Line Cooparison Computation Program");
+OOP_Line_Compare.LineCompare();
